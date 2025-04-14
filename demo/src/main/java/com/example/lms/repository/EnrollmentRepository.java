51fadfac331a0f4f98bdbe1137daccdd5949247f
Jaikumar96 <137675517+Jaikumar96@users.noreply.github.com>
2025-04-15 00:31:06 +0530

    Delete demo directory

--- demo/src/main/java/com/example/lms/repository/EnrollmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.model.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
